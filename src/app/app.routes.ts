c878a20a58f23aac356e5e9043a2098be5d5f03c
Bekir Can Ata <bekircnata@gmail.com>
2024-10-01 11:06:42 +0300

    Login page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
     {
         path: '',
         component: LayoutComponent,
