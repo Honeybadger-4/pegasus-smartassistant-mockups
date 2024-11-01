d19a01edbbee1b3e1c48c7bc47e5ee60141d67c3
Aylin Arık <aylinarik@192.168.1.118>
2024-11-01 16:27:17 +0300

    table-header

---
+++ src/app/components/flight-info/table-header/table-header.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { DropdownModule } from 'primeng/dropdown';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-table-header',
+  standalone: true,
+  imports: [CommonModule, DropdownModule, IconFieldModule, InputIconModule, FormsModule],
+  templateUrl: './table-header.component.html',
+  styleUrls: ['./table-header.component.scss'],
+})
+export class TableHeaderComponent {
+  selectedOption: string = '';
+
+  dropdownOptions = [
+    { label: 'Daily', value: 'daily' },
+
+  ];
+}
