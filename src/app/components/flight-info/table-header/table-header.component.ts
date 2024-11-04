5775d5c69b7ff914fd6bea7389f0adb5ec2ef23e
Bekir Can Ata <bekircnata@gmail.com>
2024-11-04 11:32:39 +0300

    Table header and layout responsive fix

--- src/app/components/flight-info/table-header/table-header.component.ts
+++ src/app/components/flight-info/table-header/table-header.component.ts
@@ -4,11 +4,12 @@ import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-table-header',
   standalone: true,
-  imports: [CommonModule, DropdownModule, IconFieldModule, InputIconModule, FormsModule],
+  imports: [CommonModule, DropdownModule, IconFieldModule, InputIconModule, InputTextModule, FormsModule],
   templateUrl: './table-header.component.html',
   styleUrls: ['./table-header.component.scss'],
 })
