38c08148351ba89aff9f0b58356c9a55906bb624
Bekir Can Ata <bekircnata@gmail.com>
2024-11-04 16:14:35 +0300

    Layout edit

---
+++ src/app/components/layout/sidebar-menu/sidebar-menu.component.ts
@@ -0,0 +1,91 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { LayoutMenuItem } from '@shared/models/layout-sidebar-menu';
+import { PanelModule } from 'primeng/panel';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { Sidebar } from 'primeng/sidebar';
+
+@Component({
+  selector: 'app-sidebar-menu',
+  standalone: true,
+  imports: [
+    CommonModule,
+    PanelModule,
+    MenubarModule,
+    ButtonModule,
+  ],
+  templateUrl: './sidebar-menu.component.html',
+  styleUrl: './sidebar-menu.component.scss'
+})
+export class SidebarMenuComponent {
+  menuItems: LayoutMenuItem[] = [];
+  @Input() sidebarRef!: Sidebar;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.definedMenu();
+  }
+
+  definedMenu() {
+    this.menuItems = [
+      {
+        label: 'Homepage',
+        icon: 'home-icon.svg',
+        path: '',
+      },
+      {
+        label: 'Flight Information',
+        icon: 'flight-icon.svg',
+        path: 'flight-information',
+        items: [
+          {
+            label: 'Fuel',
+            icon: 'fuel-icon.svg',
+            path: 'fuel',
+          },
+          {
+            label: 'Route',
+            icon: 'route-icon.svg',
+            path: 'route',
+          },
+          {
+            label: 'Trip Info',
+            icon: 'trip-icon.svg',
+            path: 'trip-info',
+          },
+          {
+            label: 'Load Sheet',
+            icon: 'loadsheet-icon.svg',
+            path: 'load-sheet',
+          },
+        ],
+      },
+      {
+        label: 'Airport Information',
+        icon: 'airport-icon.svg',
+        path: 'airport-information',
+      },
+      {
+        label: 'Report',
+        icon: 'report-icon.svg',
+        path: 'report',
+      },
+      {
+        label: 'User Login History',
+        icon: 'user-icon.svg',
+        path: 'user-login-history',
+      },
+      {
+        label: 'Aircraft Database',
+        icon: 'aircraft-icon.svg',
+        path: 'aircraft-database',
+      },
+    ];
+  }
+
+  closeCallback(e: any): void {
+    this.sidebarRef.close(e);
+  }
+}
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,94 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
-import { PanelModule } from 'primeng/panel';
-import { DividerModule } from 'primeng/divider';
-import { LayoutMenuItem } from '@shared/models/layout-sidebar-menu';
 import { AvatarModule } from 'primeng/avatar';
+import { SidebarMenuComponent } from "../components/layout/sidebar-menu/sidebar-menu.component";
+import { Sidebar, SidebarModule } from 'primeng/sidebar';
+
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     CommonModule,
+    SidebarMenuComponent,
     AvatarModule,
     ButtonModule,
     MenubarModule,
-    PanelModule,
-    DividerModule,
-  ],
+    SidebarModule
+],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {
-  menuItems: LayoutMenuItem[] = [];
-  selectedItem: string = '';
+  @ViewChild('sidebarRef') sidebarRef!: Sidebar;
+  sidebarVisible: boolean = false;
 
   constructor() {}
 
-  ngOnInit() {
-    this.menuItems = [
-      {
-        label: 'Homepage',
-        icon: 'home-icon.svg',
-        path: '',
-      },
-      {
-        label: 'Flight Information',
-        icon: 'flight-icon.svg',
-        path: 'flight-information',
-        items: [
-          {
-            label: 'Fuel',
-            icon: 'fuel-icon.svg',
-            path: 'fuel',
-          },
-          {
-            label: 'Route',
-            icon: 'route-icon.svg',
-            path: 'route',
-          },
-          {
-            label: 'Trip Info',
-            icon: 'trip-icon.svg',
-            path: 'trip-info',
-          },
-          {
-            label: 'Load Sheet',
-            icon: 'loadsheet-icon.svg',
-            path: 'load-sheet',
-          },
-        ],
-      },
-      {
-        label: 'Airport Information',
-        icon: 'airport-icon.svg',
-        path: 'airport-information',
-      },
-      {
-        label: 'Report',
-        icon: 'report-icon.svg',
-        path: 'report',
-      },
-      {
-        label: 'User Login History',
-        icon: 'user-icon.svg',
-        path: 'user-login-history',
-      },
-      {
-        label: 'Aircraft Database',
-        icon: 'aircraft-icon.svg',
-        path: 'aircraft-database',
-      },
-    ];
-  }
+  ngOnInit() {}
 
-  onMenuClick(label: string) {
-    this.selectedItem = label;
-  }
-
-  isActive(label: string) {
-    return this.selectedItem === label;
+  toggleSidebarMenu() {
+    this.sidebarVisible = !this.sidebarVisible;
   }
 }
