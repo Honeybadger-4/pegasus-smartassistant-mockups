eb5797d710bebc40118299ff04c366d848608329
Aylin Arık <aylinarik@192.168.1.118>
2024-11-26 11:16:41 +0300

    logbook edit breadcrumb eklendi

--- src/app/pages/logbook-edit/logbook-edit.component.ts
+++ src/app/pages/logbook-edit/logbook-edit.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+
 
 @Component({
   selector: 'app-logbook-edit',
   standalone: true,
-  imports: [],
+  imports: [BreadcrumbModule,ButtonModule],
   templateUrl: './logbook-edit.component.html',
   styleUrl: './logbook-edit.component.scss'
 })
 export class LogbookEditComponent {
-  editData: any
+  editData: any;
+  breadcrumbItems: { label: string; url?: string }[] = [];
+
 
   constructor(private route: ActivatedRoute) {}
 
@@ -19,6 +24,15 @@ export class LogbookEditComponent {
       this.editData = JSON.parse(dataParam);
       console.log(this.editData);
     }
+
+    this.breadcrumbItems = [
+      { label: 'Logbook', url: '/logbook' },
+      { label: 'Edit Logbook' }
+    ];
+  }
+
+  onSave() {
+    console.log('Save butonuna tıklandı', this.editData);
   }
 
 }
