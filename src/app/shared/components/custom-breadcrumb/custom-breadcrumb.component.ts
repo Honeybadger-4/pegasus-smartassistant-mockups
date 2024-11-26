623794874a41f72d8973657a9a0d04aa9ffb6f71
Bekir Can Ata <bekircnata@gmail.com>
2024-11-26 13:48:42 +0300

    created custom breadcrumb

---
+++ src/app/shared/components/custom-breadcrumb/custom-breadcrumb.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+
+
+@Component({
+  selector: 'app-custom-breadcrumb',
+  standalone: true,
+  imports: [BreadcrumbModule],
+  templateUrl: './custom-breadcrumb.component.html',
+  styleUrl: './custom-breadcrumb.component.scss'
+})
+export class CustomBreadcrumbComponent {
+  @Input() breadCrumbItems: MenuItem[] | undefined;
+
+}
