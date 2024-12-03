df22a2b0c4042fbd372a1f7bed99c7eda3ab903e
Bekir Can Ata <bekircnata@gmail.com>
2024-12-03 17:22:23 +0300

    test file fix

--- src/app/components/layout/menubar/menubar.component.spec.ts
+++ src/app/components/layout/menubar/menubar.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SidebarMenuComponent } from './menubar.component';
+import { MenubarComponent } from './menubar.component';
 
-describe('SidebarMenuComponent', () => {
-  let component: SidebarMenuComponent;
-  let fixture: ComponentFixture<SidebarMenuComponent>;
+describe('MenubarComponent', () => {
+  let component: MenubarComponent;
+  let fixture: ComponentFixture<MenubarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SidebarMenuComponent],
+      imports: [MenubarComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(SidebarMenuComponent);
+    fixture = TestBed.createComponent(MenubarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
