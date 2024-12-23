25a705657c2883453f4477ff78b6113408e0a3fc
Aylin Arık <aylinarik@192.168.1.118>
2024-12-23 19:02:47 +0300

    bugfiz

--- src/app/components/logbook-detail/detail-modal/detail-modal.component.ts
+++ src/app/components/logbook-detail/detail-modal/detail-modal.component.ts
@@ -1,32 +1,12 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { CommonModule } from '@angular/common'; // CommonModule'u ekleyin
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-detail-modal',
   standalone: true,
-  imports: [CommonModule], // CommonModule burada ekleniyor
+  imports: [],
   templateUrl: './detail-modal.component.html',
-  styleUrls: ['./detail-modal.component.scss'],
+  styleUrl: './detail-modal.component.scss'
 })
 export class DetailModalComponent {
-  @Output() closeModal = new EventEmitter<void>();
 
-  data = [
-    {
-      date: '22.07.2025',
-      type: 'B737',
-      reg: 'TC-IZE',
-      depPlace: 'SAW',
-      depTime: '05:05',
-      arrPlace: 'ADB',
-      arrTime: '06:14',
-      singlePilot: 'X',
-      multiPilot: '01:09',
-      totalTime: '01:09',
-    },
-  ];
-
-  close() {
-    this.closeModal.emit();
-  }
 }
