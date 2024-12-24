98ef52c6054680f8d8ca6c0a257f647b4b6e159d
Bekir Can Ata <bekircnata@gmail.com>
2024-12-24 10:52:16 +0300

    fix

--- src/app/components/logbook-detail/detail-modal/detail-modal.component.ts
+++ src/app/components/logbook-detail/detail-modal/detail-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-detail-modal',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './detail-modal.component.scss'
 })
 export class DetailModalComponent {
+  @Output() closeModal = new EventEmitter<void>();
 
+  close() {
+    this.closeModal.emit();
+  }
 }
