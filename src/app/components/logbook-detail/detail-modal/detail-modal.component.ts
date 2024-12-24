79128cff610acfd4e574940d0f4206249eb9d40d
Bekir Can Ata <bekircnata@gmail.com>
2024-12-24 16:42:07 +0300

    preview template

--- src/app/components/logbook-detail/detail-modal/detail-modal.component.ts
+++ src/app/components/logbook-detail/detail-modal/detail-modal.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common'; // CommonModule'ü import edin
 
 @Component({
   selector: 'app-detail-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detail-modal.component.html',
-  styleUrl: './detail-modal.component.scss'
+  styleUrl: './detail-modal.component.scss',
 })
 export class DetailModalComponent {
   @Output() closeModal = new EventEmitter<void>();
