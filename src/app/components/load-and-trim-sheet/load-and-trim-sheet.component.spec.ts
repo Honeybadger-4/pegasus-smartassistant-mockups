828f5b26e818874ae4037e5823f171a1ef2a5a3d
Aylin Arık <aylinarik@Aylin-MacBook-Pro.local>
2025-01-03 18:24:00 +0300

    load-and-trim-sheet oluşturuldu

---
+++ src/app/components/load-and-trim-sheet/load-and-trim-sheet.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoadAndTrimSheetComponent } from './load-and-trim-sheet.component';
+
+describe('LoadAndTrimSheetComponent', () => {
+  let component: LoadAndTrimSheetComponent;
+  let fixture: ComponentFixture<LoadAndTrimSheetComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LoadAndTrimSheetComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoadAndTrimSheetComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/load-and-trim-sheet/load-and-trim-sheet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-load-and-trim-sheet',
+  standalone: true,
+  imports: [],
+  templateUrl: './load-and-trim-sheet.component.html',
+  styleUrl: './load-and-trim-sheet.component.scss'
+})
+export class LoadAndTrimSheetComponent {
+
+}
