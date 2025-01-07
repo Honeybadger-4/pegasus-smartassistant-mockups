3d9f9429fe709f70597d4ce95cccffdd998b1200
Aylin Arık <aylinarik@Aylin-MacBook-Pro.local>
2025-01-07 16:49:33 +0300

    trip info servisi bağlandı user login filtresi düzenlendi

---
+++ src/app/shared/models/trip-info-response.model.ts
@@ -0,0 +1,39 @@
+export interface ITripInfoResponse {
+  content: [ITripInfoTableData];
+  pageable: {
+    pageNumber: number;
+    pageSize: number;
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
+  totalElements: number;
+  totalPages: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
+}
+
+export interface ITripInfoTableData {
+  username: string;
+  legIsn: number;
+  depDateTime: string;
+  depPort: string;
+  arrPort: string;
+  flightNo: string;
+  aircraftReg: string;
+  arrDateTime: string;
+}
---
+++ src/app/shared/services/trip-info.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { IHttpResponseModel } from '@shared/models/http-response.model';
+import { ITripInfoResponse } from '@shared/models/trip-info-response.model';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TripInfoService {
+  http = inject(HttpClient);
+  baseUrl = environment.baseApi;
+
+  getTripInfo(startDate: string, endDate: string, page: number, size: number): Observable<ITripInfoResponse> {
+    const apiUrl = `${this.baseUrl}/api/v1/admin/trip-info`;
+
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString());
+  
+    if (startDate && endDate) {
+      params = params.set('startDate', startDate).set('endDate', endDate);
+    }
+  
+    return this.http
+      .get<IHttpResponseModel>(apiUrl, { params })
+      .pipe(map((response) => response.data));
+  }
+  
+}
+
+
+
+
