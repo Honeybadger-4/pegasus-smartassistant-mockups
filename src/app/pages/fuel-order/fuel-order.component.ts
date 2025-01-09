5c7fdd298e8c0d01b2d6630e6f7ac74a9e6b77b8
Aylin Arık <aylinarik@Aylin-MacBook-Pro.local>
2025-01-09 14:25:30 +0300

    fuel order service connection

--- src/app/pages/fuel-order/fuel-order.component.ts
+++ src/app/pages/fuel-order/fuel-order.component.ts
@@ -1,4 +1,10 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  inject,
+  signal,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 import { DropdownModule } from 'primeng/dropdown';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
@@ -8,6 +14,12 @@ import { InputTextModule } from 'primeng/inputtext';
 import { Column } from '@shared/models/columns';
 import { CustomTableComponent } from '../../shared/components/custom-table/custom-table.component';
 import { CalendarModule } from 'primeng/calendar';
+import { FuelOrderService } from '@shared/services/fuel-order.service';
+import {
+  IFuelOrderResponse,
+  IFuelOrderTableData,
+} from '@shared/models/fuel-order-response.model';
+import moment from 'moment';
 
 @Component({
   selector: 'app-fuel',
@@ -26,45 +38,90 @@ import { CalendarModule } from 'primeng/calendar';
   styleUrl: './fuel-order.component.scss',
 })
 export class FuelOrderComponent {
+  @ViewChild(CustomTableComponent) customTableComponent!: CustomTableComponent;
   columns!: Column[];
   dateRange: Date[] = [];
 
-  fuelData = [
-    {
-      id: '0',
-      aircraft: 'TC-A329',
-      flightNo: 'PC2009',
-      departure: 'AYT',
-      arrival: 'DUS',
-      depDateTime: '22/07/2025 13:30',
-      arrDateTime: '22/07/2025 16:30',
-      amount: '4800 KG',
-    },
-    {
-      id: '1',
-      aircraft: 'TC-A329',
-      flightNo: 'PC2009',
-      departure: 'AYT',
-      arrival: 'DUS',
-      depDateTime: '22/07/2025 13:30',
-      arrDateTime: '22/07/2025 16:30',
-      amount: '4900 KG',
-    },
-  ];
+  fuelOrderService = inject(FuelOrderService);
+  fuelOrderHistoryData = signal<IFuelOrderResponse | null>(null);
+  fuelOrderHistoryTableData = signal<IFuelOrderTableData[]>([]);
+  currentPage = 0;
+  currentRows = 20;
 
   ngOnInit() {
     this.defineColumns();
+    const today = moment();
+    this.initialDateRangeValue();
   }
 
   defineColumns() {
     this.columns = [
-      { field: 'aircraft', header: 'Aircraft' },
+      { field: 'aircraftReg', header: 'Aircraft' },
       { field: 'flightNo', header: 'Flight No' },
-      { field: 'departure', header: 'Departure' },
-      { field: 'arrival', header: 'Arrival' },
+      { field: 'depPort', header: 'Departure' },
+      { field: 'arrPort', header: 'Arrival' },
       { field: 'depDateTime', header: 'Dep Date/Time' },
       { field: 'arrDateTime', header: 'Arr Date/Time' },
       { field: 'amount', header: 'Amount' },
     ];
+  }
+
+  initialDateRangeValue() {
+    const today = moment();
+    this.dateRange = [
+      today.clone().subtract(7, 'days').toDate(),
+      today.clone().add(7, 'days').toDate(),
+    ];
+  }
+
+  getFuelOrder() {
+    let startDate = '';
+    let endDate = '';
+  
+    // Tarih aralığı kontrolü ve formatlama
+    if (this.dateRange.length === 2) {
+      const [start, end] = this.dateRange;
+  
+      if (start && end) {
+        startDate = moment(start).format('YYYY-MM-DD');
+        endDate = moment(end).format('YYYY-MM-DD');
+      }
+    }
+  
+    // Filtreleme alanlarından değerleri alın
+    const acReg = (document.querySelector('input[placeholder="Aircraft"]') as HTMLInputElement)?.value || null;
+    const flightNo = (document.querySelector('input[placeholder="Flight No"]') as HTMLInputElement)?.value || null;
+    const depPort = (document.querySelector('input[placeholder="Dep Port"]') as HTMLInputElement)?.value || null;
+    const arrPort = (document.querySelector('input[placeholder="Arr Port"]') as HTMLInputElement)?.value || null;
+  
+    this.fuelOrderService
+      .getFuelOrder(this.currentPage, this.currentRows, startDate, endDate, acReg, flightNo, depPort, arrPort)
+      .subscribe({
+        next: (response) => {
+          this.fuelOrderHistoryData.set(response);
+          this.fuelOrderHistoryTableData.set(response.content);
+        },
+        error: (error) => {
+          console.error(error);
+        },
+      });
+  }
+  
+
+  onDateRangeChange(event: Event) {
+    const [startDate, endDate] = this.dateRange;
+
+    if (startDate && endDate) {
+      this.currentPage = 0;
+      this.customTableComponent.resetTableFirstValue();
+      this.getFuelOrder();
+    }
+  }
+
+  pageEvent(event: { first: number; rows: number }) {
+    const page = event.first / event.rows;
+    this.currentPage = page;
+    this.currentRows = event.rows;
+    this.getFuelOrder();
   }
 }
---
+++ src/app/shared/services/fuel-order.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { IFuelOrderResponse } from '@shared/models/fuel-order-response.model';
+import { IHttpResponseModel } from '@shared/models/http-response.model';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FuelOrderService {
+  http = inject(HttpClient);
+  baseUrl = environment.baseApi;
+
+  getFuelOrder(
+    page: number,
+    size: number,
+    startDate: string,
+    endDate: string,
+    acReg?: string | null,
+    flightNo?: string | null,
+    depPort?: string | null,
+    arrPort?: string | null,
+  ): Observable<IFuelOrderResponse> {
+    const apiUrl = `${this.baseUrl}/api/v1/admin/fuel-orders`;
+
+    let params = new HttpParams().set('page', page).set('size', size);
+
+    if (startDate && endDate) {
+      params = params.set('startDate', startDate).set('endDate', endDate);
+    }
+    if (acReg) {
+      params = params.set('acReg', acReg);
+    }
+    if (flightNo) {
+      params = params.set('flightNo', flightNo);
+    }
+    if (depPort) {
+      params = params.set('depPort', depPort);
+    }
+    if (arrPort) {
+      params = params.set('arrPort', arrPort);
+    }
+
+    return this.http
+      .get<IHttpResponseModel>(apiUrl, { params })
+      .pipe(map((response) => response.data));
+  }
+}
