9e8b867457ba04f604897a7f8145f24322f855d5
Bekir Can Ata <bekircnata@gmail.com>
2025-01-17 16:47:10 +0300

    Imza gösterimi eklendi

--- src/app/pages/logbook-detail-edit/logbook-detail-edit.component.ts
+++ src/app/pages/logbook-detail-edit/logbook-detail-edit.component.ts
@@ -286,4 +286,15 @@ export class LogbookDetailEditComponent implements OnInit {
     }
     return false;
   }
+
+  convertBase64ToImage(): string {
+    let signatureBase64 = this.editDefaultData()?.signature;
+
+    if(signatureBase64) {
+      let base64Obj = JSON.parse(signatureBase64)
+      return `data:image/jpeg;base64,${base64Obj.sign}`
+    }
+
+    return ""
+  }
 }
