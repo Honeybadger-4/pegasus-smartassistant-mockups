fec53ee0c7b254dfb90d673d4dbb447a587230d3
Bekir Can Ata <bekircnata@gmail.com>
2025-01-23 17:13:23 +0300

    Code format

--- src/app/shared/services/user-profile.service.ts
+++ src/app/shared/services/user-profile.service.ts
@@ -3,16 +3,15 @@ import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '@environments/environment';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class UserProfileService {
-   http = inject(HttpClient);
-   baseUrl = environment.baseApi; 
-   apiUrl = `${this.baseUrl}/api/v1/external/flybus/getPhoto`; 
+  http = inject(HttpClient);
+  baseUrl = environment.baseApi;
+  apiUrl = `${this.baseUrl}/api/v1/external/flybus/getPhoto`;
 
   getProfilePhoto(): Observable<Blob> {
-    return this.http.post(this.apiUrl,{}, { responseType: 'blob' });
+    return this.http.post(this.apiUrl, {}, { responseType: 'blob' });
   }
 }
