fbfb024c745b576eff3eb4fc212e5d05221ad0f2
Aylin Arık <aylinarik@Aylin-MacBook-Pro.local>
2025-01-29 14:41:04 +0300

    homepage service and interface

---
+++ src/app/shared/models/homepage-total-flights-response.model.ts
@@ -0,0 +1,10 @@
+export interface IHomepageTotalFlightsResponse {
+    totalFlights: number;
+    approvedFlightPlans: number;
+    approvedLoadSheets: number;
+    sentTripInfo: number;
+    sentFuelOrder: number;
+    lmcLoadSheets: number;
+    gpsLossForm: number;
+  }
+  
