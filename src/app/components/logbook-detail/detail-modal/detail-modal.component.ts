b801c8a2fb5bbb99572ca04bc1eaaca201f32426
Bekir Can Ata <bekircnata@gmail.com>
2025-01-30 11:32:01 +0300

    Code fix

--- src/app/components/logbook-detail/detail-modal/detail-modal.component.ts
+++ src/app/components/logbook-detail/detail-modal/detail-modal.component.ts
@@ -22,8 +22,7 @@ export class DetailModalComponent {
   detailedListRowData = input<IDetailedListContentData | null>(null);
   @Input() visible: boolean = false;
   @Output() closeModal = new EventEmitter<void>();
-  @ViewChild('tableToPdf', { static: false }) tableToPdf!: ElementRef;
-
+  
   close() {
     this.closeModal.emit();
   }
