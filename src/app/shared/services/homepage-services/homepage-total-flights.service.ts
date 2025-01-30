77203c568ed778d918d1e39a6ed1a53065fbb0b1
Aylin Arık <aylinarik@Aylin-MacBook-Pro.local>
2025-01-30 10:55:13 +0300

    bugfix

--- src/app/shared/services/homepage-services/homepage-total-flights.service.ts
+++ src/app/shared/services/homepage-services/homepage-total-flights.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
-import { IHomepageTotalFlightsResponse } from '@shared/models/homepage-total-flights-response.model';
+import { IHomepageTotalFlightsResponse } from '@shared/models/homepage-response-models/homepage-total-flights-response.model';
 import { IHttpResponseModel } from '@shared/models/http-response.model';
 import { map, Observable } from 'rxjs';
 
