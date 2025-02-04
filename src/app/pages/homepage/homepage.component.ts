f2bd652344a17ff14c3e47827657d8796d4c6d0c
Bekir Can Ata <bekircnata@gmail.com>
2025-02-04 13:38:38 +0300

    Default date filter edited

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -90,12 +90,8 @@ export class HomepageComponent {
   }
 
   dateRangeDefaultValue() {
-    // TODO: Ayın ilk ve son günü seçilmeli default olarak.
-    // const startDate = moment().startOf('month');
-    // const endDate = moment().endOf('month');
-
-    const startDate = moment(new Date());
-    const endDate = moment(new Date());
+    const startDate = moment().startOf('month');
+    const endDate = moment().endOf('month');
 
     this.startDate.set(moment(startDate).format('YYYY-MM-DD'));
     this.endDate.set(moment(endDate).format('YYYY-MM-DD'));
