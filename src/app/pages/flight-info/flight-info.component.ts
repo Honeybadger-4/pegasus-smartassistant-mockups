6647de2ece935c8137736b8b7aba6408555bdd9f
Aylin Arık <aylinarik@Aylin-MacBook-Pro.local>
2025-02-06 13:53:19 +0300

    flight info date format

--- src/app/pages/flight-info/flight-info.component.ts
+++ src/app/pages/flight-info/flight-info.component.ts
@@ -496,8 +496,15 @@ export class FlightInfoComponent {
       )
       .subscribe({
         next: (response) => {
+           const formattedData = response.content.map((item) => ({
+                      ...item,
+                      depDateTime: moment(item.depDateTime).format('DD/MM/YYYY - HH:mm'),
+                      arrDateTime: moment(item.arrDateTime).format('DD/MM/YYYY - HH:mm'),
+                        
+                    }));
+
           this.flightInformationHistoryData.set(response);
-          this.flightInformatioHistoryTableData.set(response.content);
+          this.flightInformatioHistoryTableData.set(formattedData);
           this.tableLoading = false;
         },
         error: () => {
