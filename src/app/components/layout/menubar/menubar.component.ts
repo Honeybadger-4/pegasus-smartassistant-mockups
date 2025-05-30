fbc9e382392ce717563ff373e17f8d5d73073215
Aylin Arık <arikayli1973@gmail.com>
2025-05-30 12:19:44 +0300

    feat: add Crew Information component and update routing

--- src/app/components/layout/menubar/menubar.component.ts
+++ src/app/components/layout/menubar/menubar.component.ts
@@ -112,7 +112,7 @@ export class MenubarComponent implements OnInit {
           {
             label: 'Crews',
             icon: '',
-            path: 'operational-reports/crews',
+            path: 'operational-reports/crew-information',
           },
           {
             label: 'Reports',
