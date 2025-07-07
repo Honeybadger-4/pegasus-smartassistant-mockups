c15b742449df50727a34d82df252eef6779a3f66
Aylin Arık <arikayli1973@gmail.com>
2025-07-07 19:16:01 +0300

    feat: add PDF download functionality to load and trim sheet component

--- src/app/components/load-and-trim-sheet/load-and-trim-sheet.component.ts
+++ src/app/components/load-and-trim-sheet/load-and-trim-sheet.component.ts
@@ -11,11 +11,14 @@ import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { LoadSheetService } from '@shared/services/load-sheet.service';
 import { ILoadSheetModalsResponse } from '@shared/models/load-sheet-modals-response';
-
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-load-and-trim-sheet',
   standalone: true,
-  imports: [CommonModule, DialogModule, ButtonModule],
+  imports: [CommonModule, DialogModule, ButtonModule, ToastModule],
   templateUrl: './load-and-trim-sheet.component.html',
   styleUrls: ['./load-and-trim-sheet.component.scss'],
 })
@@ -28,6 +31,7 @@ export class LoadAndTrimSheetComponent {
   raw = signal<ILoadSheetModalsResponse | null>(null);
 
   loadAndTrimSheet = inject(LoadSheetService);
+  messageService = inject(MessageService);
 
   dialogOpened() {
     const id = this.loadSheetRowData?.id;
@@ -38,6 +42,54 @@ export class LoadAndTrimSheetComponent {
     this.loadAndTrimSheet.getLoadSheetModalInfo(id).subscribe({
       next: (data) => this.raw.set(data),
       error: () => this.raw.set(null),
+    });
+  }
+  downloadAsPDF() {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Generating PDF',
+      life: 5000,
+    });
+
+    const element = document.querySelector(
+      '#LoadAndTrimSheet .content-container',
+    ) as HTMLElement;
+    if (!element) {
+      console.error('Element bulunamadı!');
+      return;
+    }
+
+    html2canvas(element, {
+      scale: 1,
+      useCORS: true,
+      logging: false,
+      allowTaint: true,
+    }).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+
+      const marginX = 10;
+      const imgWidth = pdfWidth - marginX * 2;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+      let position = 0;
+
+      let remainingHeight = imgHeight;
+
+      while (remainingHeight > 0) {
+        pdf.addImage(imgData, 'PNG', marginX, position, imgWidth, imgHeight);
+        remainingHeight -= pdfHeight;
+
+        if (remainingHeight > 0) {
+          pdf.addPage();
+          position = position - pdfHeight;
+        }
+      }
+
+      pdf.save('load-and-trim-sheet.pdf');
     });
   }
 
