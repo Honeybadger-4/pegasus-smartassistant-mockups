411ae5e07a652fe877c459a5f33397d34a8deb08
Aylin Arık <arikayli1973@gmail.com>
2025-07-24 01:39:17 +0300

    feat: add fallback toggle and enhance date range selection in dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToggleSwitch } from 'primeng/toggleswitch';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 import { KeyStatsCardComponent } from 'src/app/components/dashboard/key-stats-card/key-stats-card.component';
 import { FlightCountCardComponent } from 'src/app/components/dashboard/flight-count-card/flight-count-card.component';
 import { GpsSignalLossCardComponent } from 'src/app/components/dashboard/gps-signal-loss-card/gps-signal-loss-card.component';
 import { TotalFuelOrderedCardComponent } from 'src/app/components/dashboard/total-fuel-ordered-card/total-fuel-ordered-card.component';
 import { TopAlternateRoutesCardComponent } from 'src/app/components/dashboard/top-alternate-routes-card/top-alternate-routes-card.component';
-import { ButtonModule } from 'primeng/button';
 
 export type DateRangeType = 'today' | '1month' | '6months';
 
@@ -15,6 +17,8 @@ export type DateRangeType = 'today' | '1month' | '6months';
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
+    ToggleSwitch,
     ButtonModule,
     FlightCountCardComponent,
     TotalFuelOrderedCardComponent,
@@ -27,6 +31,8 @@ export type DateRangeType = 'today' | '1month' | '6months';
 })
 export class DashboardComponent {
   selectedRange = signal<DateRangeType>('6months');
+
+  fallbackEnabled = false;
 
   selectRange(range: DateRangeType) {
     this.selectedRange.set(range);
