7a721e437e484ec6b28d34b43c5ed1c3d9c5dcf7
Aylin Arık <arikayli1973@gmail.com>
2025-12-16 13:18:40 +0300

    feat: add logbook usage history component with basic template and style

---
+++ src/app/pages/logbook-usage-history/logbook-usage-history.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logbook-usage-history',
+  imports: [],
+  templateUrl: './logbook-usage-history.component.html',
+  styleUrl: './logbook-usage-history.component.scss'
+})
+export class LogbookUsageHistoryComponent {
+
+}
